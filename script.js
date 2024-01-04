0ea020f71a00c1b6e520b8a4df3e0f86ae6036ae
hasoni737 <hassanaitlahcen737@gmail.com>
2024-01-04 21:33:35 +0100

    Add counter tasks and in setting change color

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function fillTasksOnPage() {
         </div>
         <div class="actions">
           <button onclick="updateTask(${index})" class="circular" title="Update the task">
-            <i class="fa-solid fa-pencil"></i>
+            <i class="fa-regular fa-pen-to-square"></i>
           </button>
           ${
             task.isDone
@@ -43,7 +43,7 @@ function fillTasksOnPage() {
           `
           }
           <button onclick="deleteTask(${index})" class="circular" title="Delete the task">
-            <i class="fa-solid fa-trash"></i>
+            <i class="fa-regular fa-trash-can"></i>
           </button>
         </div>
       </div>
@@ -53,6 +53,7 @@ function fillTasksOnPage() {
   };
 };
 checkEmptyTasks();
+countTasks();
 fillTasksOnPage();
 
 //--------- Add new task on the array tasks and on the page ---------
@@ -87,6 +88,7 @@ document.querySelector("#add").addEventListener("click", function () {
       });
       storageTasks();
       checkEmptyTasks();
+      countTasks();
       fillTasksOnPage();
     }
     input.value = "";
@@ -116,6 +118,7 @@ function deleteTask(index) {
     tasks.splice(index, 1);
     storageTasks();
     checkEmptyTasks();
+    countTasks();
     fillTasksOnPage();
     delete_task.style.display = "none";
   };
@@ -167,9 +170,17 @@ function completeTask(index) {
   let task = tasks[index];
   task.isDone ? (task.isDone = false) : (task.isDone = true);
   storageTasks();
+  countTasks();
   fillTasksOnPage();
 }
 //--------------------------------------------------------------------//
+
+//======================== STORAGE FUNCTIONS ==========================
+function storageTasks() {
+  let taskString = JSON.stringify(tasks);
+  localStorage.setItem("tasks", taskString);
+};
+//=====================================================================//
 
 //------------------ Check empty tasks enter message ------------------
 function checkEmptyTasks() {
@@ -182,9 +193,138 @@ function checkEmptyTasks() {
 };
 //--------------------------------------------------------------------//
 
-//======================== STORAGE FUNCTIONS ==========================
-function storageTasks() {
-  let taskString = JSON.stringify(tasks);
-  localStorage.setItem("tasks", taskString);
-}
-//=====================================================================//
+//------------------ Count tasks and clear all tasks ------------------
+function countTasks() {
+  let countTasksNotDone = 0;
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].isDone) {
+      
+    } else {
+      countTasksNotDone += 1;
+    };
+  };
+  if (tasks.length > 0) {
+    let count = document.querySelector(".count");
+    let h3 = document.querySelector(".count h3");
+    count.style.display = "flex";
+    h3.innerHTML = `You have ${countTasksNotDone} pending tasks`;
+  } else if (tasks.length === 0) {
+    let count = document.querySelector(".count");
+    count.style.display = "none";
+  };
+};
+
+function clearAll() {
+  // -------------------------------------------
+  let clear_tasks = document.querySelector(".clear_tasks");
+  let ok = document.querySelector(".clear_tasks .btns button:first-child");
+  let cancel = document.querySelector(".clear_tasks .btns button:last-child");
+  let close = document.querySelector(".clear_tasks .close");
+  clear_tasks.style.display = "flex";
+  // Confirm delete task
+  ok.onclick = function () {
+    localStorage.clear();
+    tasks = [];
+    checkEmptyTasks();
+    countTasks();
+    fillTasksOnPage();
+    clear_tasks.style.display = "none";
+  };
+  // Cancel delete the task
+  cancel.onclick = function () {
+    clear_tasks.style.display = "none";
+  };
+  // Close the window
+  close.onclick = function () {
+    clear_tasks.style.display = "none";
+  };
+};
+//--------------------------------------------------------------------//
+
+//----------------------------- Setting ------------------------------
+let settingDiv = document.querySelector(".setting_content");
+let setting_btn = document.querySelector("#setting_btn");
+let setting_btn_hide = document.querySelector("#setting_btn_hide");
+function showSetting() {
+  setting_btn.style.display = "none";
+  setting_btn_hide.style.display = "flex";
+  settingDiv.style.display = "flex";
+};
+function hideSetting() {
+  setting_btn.style.display = "flex";
+  setting_btn_hide.style.display = "none";
+  settingDiv.style.display = "none";
+};
+//--------------------------------------------------------------------//
+
+//----------------------- Setting Change color ------------------------
+let header = document.querySelector(".header");
+let clear_btn = document.querySelector(".count #clear_btn");
+let add_btn = document.querySelector(".create_task button");
+let icons = document.querySelector(".setting_content i");
+let red = document.querySelector(".setting_content .colors #red");
+let green = document.querySelector(".setting_content .colors #green");
+let blue = document.querySelector(".setting_content .colors #blue");
+let purple = document.querySelector(".setting_content .colors #purple");
+let orange = document.querySelector(".setting_content .colors #orange");
+
+purple.onclick = function () {
+  localStorage.setItem("color", "#5c339e");
+  let color = localStorage.getItem("color");
+  header.style.backgroundColor = color;
+  clear_btn.style.backgroundColor = color;
+  add_btn.style.backgroundColor = color;
+  icons.style.color = color;
+  setting_btn.style.display = "flex";
+  setting_btn_hide.style.display = "none";
+  settingDiv.style.display = "none";
+};
+red.onclick = function () {
+  localStorage.setItem("color", "#E31B24");
+  let color = localStorage.getItem("color");
+  header.style.backgroundColor = color;
+  clear_btn.style.backgroundColor = color;
+  add_btn.style.backgroundColor = color;
+  icons.style.color = color;
+  setting_btn.style.display = "flex";
+  setting_btn_hide.style.display = "none";
+  settingDiv.style.display = "none";
+};
+green.onclick = function () {
+  localStorage.setItem("color", "#05AC72");
+  let color = localStorage.getItem("color");
+  header.style.backgroundColor = color;
+  clear_btn.style.backgroundColor = color;
+  add_btn.style.backgroundColor = color;
+  icons.style.color = color;
+  setting_btn.style.display = "flex";
+  setting_btn_hide.style.display = "none";
+  settingDiv.style.display = "none";
+};
+blue.onclick = function () {
+  localStorage.setItem("color", "#4188FF");
+  let color = localStorage.getItem("color");
+  header.style.backgroundColor = color;
+  clear_btn.style.backgroundColor = color;
+  add_btn.style.backgroundColor = color;
+  icons.style.color = color;
+  setting_btn.style.display = "flex";
+  setting_btn_hide.style.display = "none";
+  settingDiv.style.display = "none";
+};
+orange.onclick = function () {
+  localStorage.setItem("color", "#FF7400");
+  let color = localStorage.getItem("color");
+  header.style.backgroundColor = color;
+  clear_btn.style.backgroundColor = color;
+  add_btn.style.backgroundColor = color;
+  icons.style.color = color;
+  setting_btn.style.display = "flex";
+  setting_btn_hide.style.display = "none";
+  settingDiv.style.display = "none";
+};
+header.style.backgroundColor = localStorage.getItem("color");
+clear_btn.style.backgroundColor = localStorage.getItem("color");
+add_btn.style.backgroundColor = localStorage.getItem("color");
+icons.style.color = localStorage.getItem("color");
+//--------------------------------------------------------------------//
